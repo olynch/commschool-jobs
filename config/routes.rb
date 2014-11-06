24c74db92aceb6ae277fb722a5e3a960eea38fed
Owen Lynch <oclynch888@gmail.com>
2014-11-06 09:28:58 -0500

    crew/weeks resource routes disabled

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-	resources :weeks
-	resources :crews
+	#resources :weeks
+	#resources :crews
 
 	root 'welcome#index'
 
